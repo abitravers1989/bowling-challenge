ea0d076a0490af6695bdbc06133af89038c67e66
Abi Travers <atravers4007@gmail.com>
2017-09-02 15:13:06 +0100

    Attempted making a spy but stubbed randomness out different way

--- spec/bowling_spec.js
+++ spec/bowling_spec.js
@@ -34,10 +34,12 @@ describe("Bowling", function(){
 
      beforeEach(function(){
        bowling = new Bowling();
+      //  var randomnumber = jasmine.createSpyObj("randomnumber", 8)
       //  randomnumber = jasmine.createSpyObj('randomnumber', return 8)
      });
 
      it("The first roll changes the number of pins and the total score", function(){
+       this.randomnumber = 8
        bowling.roll1();
        expect(bowling.pinsnr).toEqual(2)
        expect(bowling.currentscore).toEqual(8)
