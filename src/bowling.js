fb2d5a6e08533a0249c694af3bd27566aa095886
Abi Travers <atravers4007@gmail.com>
2017-09-02 17:24:32 +0100

    First attempt at adding logic of the game

--- src/bowling.js
+++ src/bowling.js
@@ -6,20 +6,33 @@ var Bowling = function(){
   this.currentscore = 0
   this.totalscore = []
   this.pinsnr = 10
+  this.strike = false
+  this.bonus = 5
 }
 
-var randomnumber = function() {
+ var randomnumber = function() {
    return 8
   //  BOWLNUM = [1,2,3,4,5,6,7,8,9,10]
   //  BOWLNUM._sample
-};
+ };
 
-    // Bowling.prototype.gameframe = function(){
-    //   roll1();
-    //   if (roll1(); < 10 && strike === false)
-    // };
+  Bowling.prototype.gameframe = function(){
+    // while this.framenr >= 10
+      roll1();
+      if (roll1(); < 10){
+        roll2();
+      } else {
+        this.strike = true
+        this.totalscore.push(10)
+      }
+    };
 
   Bowling.prototype.roll1 = function(){
+    this.pinsnr -= randomnumber()
+    this.currentscore += randomnumber()
+  };
+
+  Bowling.prototype.roll2 = function(){
     this.pinsnr -= randomnumber()
     this.currentscore += randomnumber()
   };
