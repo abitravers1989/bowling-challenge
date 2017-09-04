ef410b61d30b0fa983ac1f9ed88e0280d4694531
Abi Travers <atravers4007@gmail.com>
2017-09-04 19:06:39 +0100

    Further thought process

--- src/score.js
+++ src/score.js
@@ -1,11 +1,12 @@
 
 var Score = function(){
   this.totalscore = []
-  this.framenr = 1
-  this.strike = {};
+  this.strike = [];
   this.spare = {};
+  this.frame = 1
 };
 
+// maybe don't need frame number? just a pattern of true and falses then compare this to score array
 Score.prototype.hasstrike = function(bowlingstrike){
       frame = 1
       console.log(bowlingstrike)
@@ -15,12 +16,19 @@ Score.prototype.hasstrike = function(bowlingstrike){
     } else {
       this.strike[frame] = false;
     }
-    frame += 1
+    nextframe = frame += 1
+    this.frame = {nextframe}
     console.log(frame)
+    // when called multiple times it only puts first pair in strike so this
+    // isn't a hash
+    console.log(this.strike)
   };
 
 scor = new Score
 scor.hasstrike(true)
+scor.hasstrike(false)
+scor.strike
+scor.frame
 
   Score.prototype.calculatingframe = function(roll1, strike, roll2, spare){
     score1 = roll1 + roll2
