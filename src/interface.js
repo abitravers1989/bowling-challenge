08758172ef6795099e5b00795ad4a6f66bcb46aa
Abi Travers <atravers4007@gmail.com>
2017-09-09 16:33:35 +0100

    First attempts at adding interface and functionality

--- src/interface.js
+++ src/interface.js
@@ -2,7 +2,19 @@ $( document ).ready(function(){
   var bowling = new Bowling
   var score = new Score
 
+function updatescore(){
 $('#currentscore').text('Your current score is' + score.calculatingtotalscorewithbonus(); + " Ratpigeons")
+}
+
+function updatecoreafterroll(){
+  $('#currentscore').attr('class', score.calculatingtotalscorewithbonus())
+}
+
+$('#frame1').on( 'click', function() {
+  score.calculatingframe(10, false, 0, false);
+  calculatingtotalscorewithbonus();
+})
+
 
 // function frame1(){
 // $('#roll1')
