30125606d5bab385ba98556934e97ca94acf66ce
Abi Travers <atravers4007@gmail.com>
2017-09-11 08:59:36 +0100

    Functionality for adding first bowl to the interface with error for trying to roll two balls from the same roll option, added

--- interface.js
+++ interface.js
@@ -23,20 +23,24 @@ function updateframenr(){
   score1.framenr += 1
 };
 
-//
-// $('#frame1option1').on( 'click', function(){
-//   score1.calculatingframe(2, false, 3, false);
-//   score1.calculatingtotalscorewithbonus();
-//   updatescore();
-//   updateframenr();
-// });
-//
-// $('#frame1option2').on( 'click', function(){
-//   score1.calculatingframe(10, true, 3, false);
-//   score1.calculatingtotalscorewithbonus();
-//   strikemessage();
-//   updateframenr();
-// });
+
+$('#frame1option1').on( 'click', function(){
+  score1.calculatingframe(2, false, 3, false);
+  score1.calculatingtotalscorewithbonus();
+  updatescore();
+  updateframenr();
+});
+
+$('#frame1option2').on( 'click', function(){
+  if (score1.score != 0 ) {
+    window.alert("You have already thrown your first ball, please throw second!")
+  }
+  score1.calculatingframe(10, true, 3, false);
+  score1.calculatingtotalscorewithbonus();
+  strikemessage();
+  updateframenr();
+
+});
 //
 // $('#frame2option1').on( 'click', function(){
 //   score1.calculatingframe(2, false, 3, false);
